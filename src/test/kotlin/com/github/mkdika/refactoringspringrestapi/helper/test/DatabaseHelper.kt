1b6f648a90ca193a29e001496f2ef33aa94c0043
Maikel Chandika <mkdika@gmail.com>
2020-02-22 16:25:57 +0700

    Add generate Person database helper

--- src/test/kotlin/com/github/mkdika/refactoringspringrestapi/helper/test/DatabaseHelper.kt
+++ src/test/kotlin/com/github/mkdika/refactoringspringrestapi/helper/test/DatabaseHelper.kt
@@ -1,9 +1,11 @@
 package com.github.mkdika.refactoringspringrestapi.helper.test
 
+import com.github.javafaker.Faker
 import com.github.mkdika.refactoringspringrestapi.model.Person
 import com.github.mkdika.refactoringspringrestapi.repository.PersonRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+
 
 @Component
 class DatabaseHelper {
@@ -19,4 +21,16 @@ class DatabaseHelper {
         personRepository.save(person)
     }
 
+    fun generatePersonsData(size: Int) {
+        val faker = Faker()
+        (1..size).forEach { number ->
+            val p = Person(
+                    id = number,
+                    firstName = faker.name().firstName(),
+                    lastName = faker.name().lastName(),
+                    email = faker.internet().emailAddress()
+            )
+            personRepository.save(p)
+        }
+    }
 }
